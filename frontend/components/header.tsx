b09547a65f6e863386406a59e258e99107776d6e
DegenMario <degenmario18@gmail.com>
2021-12-23 18:16:50 -0500

    Changes to hero section

--- frontend/components/header.tsx
+++ frontend/components/header.tsx
@@ -1,49 +1,26 @@
-
-import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
-import { MenuIcon, XIcon } from '@heroicons/react/outline'
-import { ChevronRightIcon } from '@heroicons/react/solid'
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { ChevronRightIcon } from "@heroicons/react/solid";
 
 const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
-]
+  { name: "About", href: "#" },
+  { name: "Tokenomics", href: "#" },
+  { name: "Roadmap", href: "#" },
+  { name: "Chart", href: "#" },
+  { name: "How to Buy", href: "#" },
+];
 
 export default function header() {
   return (
-    <div className="relative bg-gradient-to-r from-blue-900 to-blue-400 overflow-hidden">
-      <div className="hidden sm:block sm:absolute sm:inset-0" aria-hidden="true">
-        <svg
-          className="absolute bottom-0 right-0 transform translate-x-1/2 mb-48 text-gray-700 lg:top-0 lg:mt-28 lg:mb-0 xl:transform-none xl:translate-x-0"
-          width={364}
-          height={384}
-          viewBox="0 0 364 384"
-          fill="none"
-        >
-          <defs>
-            <pattern
-              id="eab71dd9-9d7a-47bd-8044-256344ee00d0"
-              x={0}
-              y={0}
-              width={20}
-              height={20}
-              patternUnits="userSpaceOnUse"
-            >
-              <rect x={0} y={0} width={4} height={4} fill="currentColor" />
-            </pattern>
-          </defs>
-          <rect width={364} height={384} fill="url(#eab71dd9-9d7a-47bd-8044-256344ee00d0)" />
-        </svg>
-      </div>
+    <div className="relative bg-blue-400 overflow-hidden">
       <div className="relative pt-6 pb-16 sm:pb-24">
         <Popover>
           <nav
             className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
             aria-label="Global"
           >
-            <div className="flex items-center flex-1">
+            <div className="flex items-center">
               <div className="flex items-center justify-between w-full md:w-auto">
                 <a href="#">
                   <span className="sr-only">Workflow</span>
@@ -62,22 +39,17 @@ export default function header() {
               </div>
               <div className="hidden space-x-10 md:flex md:ml-10">
                 {navigation.map((item) => (
-                  <a key={item.name} href={item.href} className="font-medium text-white hover:text-gray-300">
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="font-medium text-white hover:text-gray-300"
+                  >
                     {item.name}
                   </a>
                 ))}
               </div>
             </div>
-            <div className="hidden md:flex">
-              <a
-                href="#"
-                className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700"
-              >
-                Log in
-              </a>
-            </div>
           </nav>
-
 
           <Transition
             as={Fragment}
@@ -130,42 +102,32 @@ export default function header() {
           </Transition>
         </Popover>
 
-        <main className="mt-16 sm:mt-24">
+        <main className="mt-10 md:mt-20">
           <div className="mx-auto max-w-7xl">
-            <div className="lg:grid lg:grid-cols-12 lg:gap-8">
-              <div className="px-4 sm:px-6 sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left lg:flex lg:items-center">
+            <div className="md:grid md:grid-cols-12 md:gap-8">
+              <div className="px-4 sm:px-6 md:max-w-2xl md:mx-10px md:col-span-6 md:text-left flex">
                 <div>
-                  <a
-                    href="#"
-                    className="inline-flex items-center text-white bg-gray-900 rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200"
-                  >
-                    <span className="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-indigo-500 rounded-full">
-                      We're hiring
+                  <h1 className=" text-4xl tracking-normal font-extrabold text-white sm:mt-5 sm:leading-none md:mt-10 lg:text-5xl xl:text-6xl">
+                    <span className="md:block">
+                      Meet SURFER INU, the Inu surfing its way through the
+                      Metaverse!
                     </span>
-                    <span className="ml-4 text-sm">Visit our careers page</span>
-                    <ChevronRightIcon className="ml-2 w-5 h-5 text-gray-500" aria-hidden="true" />
-                  </a>
-                  <h1 className="mt-4 text-4xl tracking-tight font-extrabold text-white sm:mt-5 sm:leading-none lg:mt-6 lg:text-5xl xl:text-6xl">
-                    <span className="md:block">Data to enrich your</span>{' '}
-                    <span className="text-indigo-400 md:block">online business</span>
                   </h1>
-                  <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
-                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
-                    amet fugiat veniam occaecat fugiat aliqua ad ad non deserunt sunt.
+                  <p className="mt-3 text-base text-white sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+                    Surfer Inu ($SURFINU) is a highly deflationary token on the
+                    Ethereum Blockchain that never stops pumping through the use
+                    of true deflationary techniques and further income
+                    generation.
                   </p>
-                
                 </div>
               </div>
               <div className="mt-16 sm:mt-24 lg:mt-0 lg:col-span-6">
-                <div className="bg-white sm:max-w-md sm:w-full sm:mx-auto sm:rounded-lg sm:overflow-hidden">
-  
-
-                </div>
+                <div className="bg-white sm:max-w-md sm:w-full sm:mx-auto sm:rounded-lg sm:overflow-hidden"></div>
               </div>
             </div>
           </div>
         </main>
       </div>
     </div>
-  )
+  );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          400: '#1085D9',
+        }
+      }
+    },
   },
   plugins: [
     // ...

77eb25c770d18977c4d8172990c740ce3edbce28
DegenMario <degenmario18@gmail.com>
2021-12-24 12:18:44 -0500

    Added layout for about page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,11 @@
-import Header from '../components/header'
-import Features from '../components/features'
-
+import Hero from "../components/hero";
+import About from "../components/about";
 
 export default function Index() {
-  return <>
-    <Header />
-    <Features />
-  </> 
+  return (
+    <body>
+      <Hero />
+      <About/>
+    </body>
+  );
 }

7cbfeae2a8dd9b34aaf1b39339a3faade30a3095
brihu23 <brihu.sundararaman@yale.edu>
2021-12-23 22:46:27 -0500

    tokenomics svg

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,16 @@
 import Header from '../components/header'
 import Features from '../components/features'
+import Tokenomics from '../assets/tokenomics'
+
 
 
 export default function Index() {
   return <>
     <Header />
     <Features />
+    <div className='bg-blue-400 flex justify-center py-16'>
+      <Tokenomics />
+    </div>
+
   </> 
 }

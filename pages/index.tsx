282886d8641e48375a094b691fa2c9d8e1863123
degenmanatee <degenmanatee@gmail.com>
2021-12-27 01:34:51 -0500

    meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,18 @@ import Nav from "../components/nav";
 import About from "../components/about";
 import Project from "../components/project";
 import Community from "../components/community";
+import Head from 'next/head'
 
 
 export default function Index() {
   return (
+    <>
+    <Head>
+        <title>Surfer Inu</title>
+        <meta name='description' content='Surfer Inu ($SURFINU) is a highly deflationary token on the Ethereum Blockchain that never stops pumping through the use of true deflationary techniques and further income generation.'></meta>
+        <meta property="og:image"  content="./meta_image.png"></meta>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <body>
       <Nav/>
       <Hero1/>
@@ -17,5 +25,6 @@ export default function Index() {
       <Tokenomics/>
       <Community /> 
     </body>
+    </>
   );
 }
